a3e3c0540dcab17135f667008ddf2dca0efa34b5
Bengt Sjögren <Bengan92@gmail.com>
2024-11-19 21:56:31 -0600

    monitoring stack incl hw

--- kubernetes/monitoring/kube-prometheus-stack/upgrade.sh
+++ kubernetes/monitoring/kube-prometheus-stack/upgrade.sh
@@ -1,5 +1,7 @@
+SCRIPT_PATH="kubernetes/monitoring/kube-prometheus-stack"
+kubectl apply -f ${SCRIPT_PATH}/pi-temp-conf.yaml
 helm upgrade \
     --namespace monitoring \
     --create-namespace \
     --install monitoring prometheus-community/kube-prometheus-stack \
-    -f kubernetes/monitoring/kube-prometheus-stack/values.yaml
+    -f ${SCRIPT_PATH}/values.yaml
