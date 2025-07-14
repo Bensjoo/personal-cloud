af0a180ba5416b9c3ae9593aed4ce99bde23302c
Bengt Sjögren <Bengan92@gmail.com>
2025-07-14 09:38:27 -0500

    Upgrade airflow chart

--- services/scheduler/create-web-secrets.sh
+++ services/scheduler/create-web-secrets.sh
@@ -1,5 +1,5 @@
 NS=scheduler
 WEBSERVER_SECRET=$(op read op://vandelay/airflow/webserver)
-kubectl create secret generic webserver-key \
-    --from-literal="webserver-secret-key=$WEBSERVER_SECRET" \
+kubectl create secret generic api-key \
+    --from-literal="api-secret-key=$WEBSERVER_SECRET" \
     --namespace=$NS
